62c9cd23b427c47123369ac1f24849285c05080e
kapiljangid8290 <kapiljangidcds@gmail.com>
2025-12-27 00:47:44 +0530

    Day 2: js and basic logic

---
+++ script.js
@@ -0,0 +1,16 @@
+// Variables
+let name = "Kapil";
+let experience = 1;
+let isLearning = true;
+
+// Output
+console.log("Name:", name);
+console.log("Experience:", experience);
+console.log("Learning JS:", isLearning);
+
+// Simple logic
+if (experience < 2) {
+  console.log("Status: Beginner, but improving fast 🚀");
+} else {
+  console.log("Status: Intermediate");
+}
